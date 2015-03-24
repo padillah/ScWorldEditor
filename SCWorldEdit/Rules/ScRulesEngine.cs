ce36143a86807eeb2fc5b43ed42bea5d492a393f
Henry Padilla <padilla.henry@gmail.com>
2015-03-24 12:32:16 -0500

    Added rules engine and ScWorld classes.

---
+++ SCWorldEdit/Rules/ScRulesEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib;
+
+namespace SCWorldEdit.Rules
+{
+	public interface IScRulesEngine
+	{
+		ScWorld LoadWorld(string argFileName);
+	}
+
+	public class ScRulesEngine : IScRulesEngine
+	{
+		public ScWorld LoadWorld(string argFileName)
+		{
+			//Create Temp directory
+			DirectoryInfo localDirectory = Directory.CreateDirectory("ScWorldEdit");
+			string newFileName = localDirectory.FullName + "\\Temp.zip";
+
+			File.Copy(argFileName, newFileName, true);
+
+			//Open the zip file.
+			//Save the three files to the Temp dir
+			ZipWrapper.ExtractZipFile(newFileName, localDirectory.FullName);
+			
+			//Open the "Chunks.dat" file.
+			string chunkFileName = localDirectory.FullName + "\\Chunks.dat";
+			ScWorld localWorld = new ScWorld(chunkFileName);
+
+			var fileStream = File.Open(chunkFileName, FileMode.Open, FileAccess.ReadWrite);
+			//var writer = new BinaryWriter(fileStream);
+			var file = new BinaryReader(fileStream);
+
+			for (int i = 0; i < 65536; i++)
+			{
+				//TODO: Fill ScWorld class.
+				localWorld.AddDirectoryEntry(file.ReadInt32(), file.ReadInt32(), file.ReadInt32());
+			}
+
+			return localWorld;
+		}
+	}
+}
---
+++ SCWorldEdit/Rules/ScWorld.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace SCWorldEdit.Rules
+{
+	public class ScWorld
+	{
+		public List<DirectoryEntry> DirectoryEntries { get; set; }
+		public string FileName { get; private set; }
+		
+		public ScWorld(string argFileName)
+		{
+			FileName = argFileName;
+			DirectoryEntries = new List<DirectoryEntry>();
+		}
+
+		public void AddDirectoryEntry(int argChunkX, int argChunkY, int argOffset)
+		{
+			DirectoryEntries.Add(new DirectoryEntry(argChunkX,argChunkY,argOffset));
+		}
+	}
+
+	public class DirectoryEntry
+	{
+		public int ChunkX { get; private set; }
+		public int ChunkY { get; private set; }
+		public int Offset { get; private set; }
+
+		public DirectoryEntry(int argChunkX, int argChunkY, int argOffset)
+		{
+			ChunkX = argChunkX;
+			ChunkY = argChunkY;
+			Offset = argOffset;
+		}
+	}
+}
