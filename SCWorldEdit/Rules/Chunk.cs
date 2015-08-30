ca77f56a43dba7a83b594349d60cbf065b00f1fd
qnnnnez <qnnnnez@live.com>
2015-08-30 18:37:55 +0800

    Implement Chunk.

--- SCWorldEdit/Rules/Chunk.cs
+++ SCWorldEdit/Rules/Chunk.cs
@@ -1,7 +1,55 @@
+using System;
+
 namespace SCWorldEdit.Rules
 {
+    public struct Block
+    {
+        public Byte BlockType;
+        public Byte BlockData;
+
+        public Block(Byte blockType, Byte blockData)
+        {
+            BlockType = blockType;
+            BlockData = blockData;
+        }
+    }
+
     public class Chunk
     {
+        public Int32 ChunkX { get; private set; }
+        public Int32 ChunkY { get; private set; }
+        const int SizeX = 16;
+        const int SizeY = 128;
+        const int SizeZ = 16;
+        public Block[] Blocks { get; private set; }
 
+        public Chunk(Int32 chunkX = -1, Int32 chunkY = -1)
+        {
+            ChunkX = chunkX;
+            ChunkY = chunkY;
+            Blocks = new Block[SizeX * SizeY * SizeZ];
+            for (var i = 0; i < Blocks.Length; ++i)
+            {
+                Blocks[i].BlockType = 0;// Air
+                Blocks[i].BlockData = 0;// No data
+            }
+        }
+
+        private int CalculateBlockIndex(int x, int y, int z)
+        {
+            return y + x * SizeY + z * SizeY * SizeX;
+        }
+
+        public Block GetBlockInChunk(int x, int y, int z)
+        {
+            int index = CalculateBlockIndex(x, y, z);
+            return Blocks[index];
+        }
+
+        public void SetBlockInChunk(int x, int y, int z, Block block)
+        {
+            int index = CalculateBlockIndex(x, y, z);
+            Blocks[index] = block;
+        }
     }
 }
