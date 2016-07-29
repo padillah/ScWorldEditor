800060fcce39bc6b2be5610d4236105def451b8d
Henry Padilla <padilla.henry@gmail.com>
2016-07-28 19:20:18 -0500

    Adding MsBuild path so bat can be run from standard CLI.

--- SonarBuild.bat
+++ SonarBuild.bat
@@ -1,4 +1,4 @@
-path=%path%;C:\sonarqube-5.6.1\MSBuild.SonarQube.Runner-2.1
+path=C:\sonarqube-5.6.1\MSBuild.SonarQube.Runner-2.1;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow;C:\Program Files (x86)\MSBuild\14.0\bin;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\BIN;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools;C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\VCPackages;C:\Program Files (x86)\HTML Help Workshop;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Performance Tools;C:\Program Files (x86)\Windows Kits\10\bin\x86;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\;%path%
 
 msbuild.sonarQube.runner begin /n:ScWorldEditor /k:ScWorldEditorKey /v:%1
 
