01bd22f18be2007bbc6baf629ab5735d2401588b
Henry Padilla <padilla.henry@gmail.com>
2016-07-31 16:01:06 -0500

    Settings for SonarQube to limit analysis to this project only.

--- SonarBuild.bat
+++ SonarBuild.bat
@@ -1,7 +1,14 @@
-path=C:\sonarqube-5.6.1\MSBuild.SonarQube.Runner-2.1;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow;C:\Program Files (x86)\MSBuild\14.0\bin;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\BIN;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools;C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\VCPackages;C:\Program Files (x86)\HTML Help Workshop;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Performance Tools;C:\Program Files (x86)\Windows Kits\10\bin\x86;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\;%path%
+@ECHO OFF
+SETLOCAL
+SET sqName=ScWorldEdit
 
-msbuild.sonarQube.runner begin /n:ScWorldEditor /k:ScWorldEditorKey /v:%1
+SET path=C:\sonarqube-5.6.1\MSBuild.SonarQube.Runner-2.1;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow;C:\Program Files (x86)\MSBuild\14.0\bin;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\BIN;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools;C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\VCPackages;C:\Program Files (x86)\HTML Help Workshop;C:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Performance Tools;C:\Program Files (x86)\Windows Kits\10\bin\x86;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\;%path%
 
-msbuild
+SET sqKey=%sqName%Key
+SET sqVersion=%1
 
-msbuild.sonarQube.runner end
+msbuild.sonarQube.runner begin /n:%sqName% /k:%sqKey% /v:%sqVersion%
+
+msbuild /t:rebuild /fl /p:SQProjectKey=%sqKey%
+
+msbuild.sonarQube.runner end > SonarResults.log
